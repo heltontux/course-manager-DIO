f1f1b9682ca49f40d67c5d87ea843084acce2001
Helton Augusto <heltontux@gmail.com>
2020-09-30 00:04:00 -0300

    Projeto similar ao original escrito do zero

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+
+import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { CourseListComponent } from './courses/course-list.component';
+import { StarComponent } from './courses/star/star.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    StarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/courses/star/star.component.ts
@@ -0,0 +1,18 @@
+import { componentFactoryName, templateJitUrl } from '@angular/compiler';
+import { Component, OnChanges, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-star',
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css']
+})
+export class StarComponent implements OnChanges {
+
+    @Input()
+    rating: number = 0;
+    starwidth: number;
+
+    ngOnChanges(): void {
+        this.starwidth = this.rating * 74/5;
+    }
+}
